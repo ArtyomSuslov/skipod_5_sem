34037bdbd5afb32ce9a9103c19c2a56c0d0eef16
Suslov Artyom <suslov_msu@mail.ru>
2024-11-28 23:58:42 +0300

    New for

--- var91_openmp_for.c
+++ var91_openmp_for.c
@@ -24,31 +24,41 @@ int main(int an, char **as)
 {
 	int it;
 
-	// 
-	double A [N][N][N],  B [N][N][N];
+	int threads[18] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 40, 60, 80, 100, 120, 140, 160};
+	double threads_time[18];
+	int threads_iter;
+    double timer_start;
+    double timer_end;
+
+	double A[N][N][N], B[N][N][N];
 	
-	init(A);
-
-	// starting timer
-	double timer_start = omp_get_wtime();
-
-	for(it=1; it<=itmax; it++)
+	for (threads_iter=0; threads_iter<18; threads_iter++) 
 	{
-		eps = 0.;
-		relax(A, B);
-		resid(A, B);
-		printf( "it=%4i   eps=%f\n", it,eps);
-		if (eps < maxeps) break;
+		double timer_start = omp_get_wtime();
+
+		init(A);
+
+		for(it=1; it<=itmax; it++)
+		{
+			eps = 0.;
+			relax(A, B);
+			resid(A, B);
+			// printf( "it=%4i   eps=%f\n", it,eps);
+			if (eps < maxeps) break;
+		}
+
+		verify(A);
+
+		double timer_end = omp_get_wtime();
+		double time_spent = timer_end - timer_start;
+
+		threads_time[threads_iter] = time_spent;
 	}
 
-	// stopping timer after relax() + resid()
-	double timer_end = omp_get_wtime();
-	double time_spent = timer_end - timer_start;
-
-	verify(A);
-
-	// printing the calculated time
-	printf("relax() + resid() were running for {%f} seconds with N = {%d}\n", time_spent, N);
+	for (threads_iter=0; threads_iter<18; threads_iter++) 
+	{
+		printf("For N = [%d] and [%d] threads: [%f] seconds\n", 
+		       N, threads[threads_iter], threads_time[threads_iter]);
 
 	return 0;
 }
@@ -57,7 +67,7 @@ void init(double A [N][N][N])
 {
 	int i,j,k;
 
-	#pragma omp parallel for default(none) shared(A) private(i, j, k)	
+	#pragma omp parallel for default(none) shared(A) private(i, j, k)
 	for(i=0; i<=N-1; i++)
 	for(j=0; j<=N-1; j++)
 	for(k=0; k<=N-1; k++)
@@ -103,6 +113,7 @@ void verify(double A [N][N][N])
 
 	double s;
 	s=0.;
+	#pragma omp parallel for default(none) shared(A) private(i, j, k) reduction(+:s) 
 	for(i=0; i<=N-1; i++)
 	for(j=0; j<=N-1; j++)
 	for(k=0; k<=N-1; k++)
