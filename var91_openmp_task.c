b797f3d0f6597ecf29ad2c472027bacb84e1b793
Suslov Artyom <suslov_msu@mail.ru>
2024-11-29 00:52:44 +0300

    y

--- var91_openmp_task.c
+++ var91_openmp_task.c
@@ -53,6 +53,57 @@ int main(int an, char **as)
     return 0;
 }
 
+int main(int an, char **as)
+{
+	int it;
+
+	int threads[18] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 40, 60, 80, 100, 120, 140, 160};
+	double threads_time[18];
+	int threads_iter;
+    double timer_start, timer_end;
+
+	double A[N][N][N], B[N][N][N];
+	
+	for (threads_iter=0; threads_iter<18; threads_iter++) 
+	{
+		omp_set_num_threads(threads[threads_iter]);
+
+		double timer_start = omp_get_wtime();
+
+		init(A);
+
+		#pragma omp parallel shared(A, B, eps)
+    	{
+			#pragma omp single
+        	{
+				for(it=1; it<=itmax; it++)
+				{
+					eps = 0.;
+					relax(A, B);
+					resid(A, B);
+					//printf( "it=%4i   eps=%f\n", it,eps);
+					if (eps < maxeps) break;
+				}
+			}
+		}
+
+		verify(A);
+
+		double timer_end = omp_get_wtime();
+		double time_spent = timer_end - timer_start;
+
+		threads_time[threads_iter] = time_spent;
+	}
+
+	for (threads_iter=0; threads_iter<18; threads_iter++) 
+	{
+		printf("For N = [%d] and [%d] threads: [%f] seconds\n", 
+		       N, threads[threads_iter], threads_time[threads_iter]);
+	}
+
+	return 0;
+}
+
 void init(double A[N][N][N])
 {
     int i, j, k;
@@ -109,8 +160,8 @@ void resid(double A[N][N][N], double B[N][N][N])
 	for (i=0; i<omp_get_max_threads(); i++)
         eps = Max(eps, eps_threads[i]);
     
-	free(eps_threads);
-	// memset(eps_threads, 0, omp_get_max_threads() * sizeof(double));
+	//free(eps_threads);
+	memset(eps_threads, 0, omp_get_max_threads() * sizeof(double));
 }
 
 void verify(double A[N][N][N])
